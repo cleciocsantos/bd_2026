49802c8d9418f3a78d3b36281e8c1c8aa364fc22
Clécio Santos <santosc.clecio@gmail.com>
2026-06-19 13:34:28 -0300

    Gabarito da Atividade Avaliativa 7

---
+++ Atividades Avaliativas/Atividade Avaliativa 7/Gabarito da ATV7/biblioteca.sql
@@ -0,0 +1,34 @@
+-- 1
+SELECT livro.titulo as Livro, 
+       autor.nome AS Autor
+FROM livro
+INNER JOIN autor ON livro.autorId = autor.autorId;
+
+-- 2
+SELECT livro.titulo as Livro, 
+       livro.anoPublicacao as 'Ano de Publicação'
+FROM livro
+WHERE livro.anoPublicacao > 2010;
+
+-- 3
+SELECT usuario.nome as Usuário, 
+       livro.titulo as 'Livro Emprestado'
+FROM emprestimo
+INNER JOIN usuario ON emprestimo.usuarioId = usuario.usuarioId
+INNER JOIN livro ON emprestimo.livroId = livro.livroId;
+
+-- 4
+SELECT usuario.nome as Usuário, 
+       livro.titulo as 'Livro Emprestado', 
+       emprestimo.dataEmp as 'Data de Empréstimo'
+FROM emprestimo
+INNER JOIN usuario ON emprestimo.usuarioId = usuario.usuarioId
+INNER JOIN livro ON emprestimo.livroId = livro.livroId
+WHERE emprestimo.dataDev IS NULL;
+
+-- 5
+SELECT autor.nome as Autor, 
+       COUNT(livro.livroId) AS 'Quantidade de Livros'
+FROM autor
+LEFT JOIN livro ON autor.autorId = livro.autorId
+GROUP BY autor.nome;
